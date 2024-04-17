6548e08d89f5600f71bf78c7912b1e383801e0a0
seo1v <165138611+seo1v@users.noreply.github.com>
2024-04-17 16:55:17 +0900

    Comment on modified date and author

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -6,7 +6,7 @@ python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/train -o [PATH_TO_ANNOTATIONS_FO
 # Create test data:
 python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/test -o [PATH_TO_ANNOTATIONS_FOLDER]/test_labels.csv
 """
-#modifed by Prof. Kim 2024. 03. 22
+#modifed by Student Eum 2024. 04. 17
 
 import os
 import glob
